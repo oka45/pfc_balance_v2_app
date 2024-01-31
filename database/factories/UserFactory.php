62b55b6663eff7b2cf10c4ae94f79b6514c008c3
okachan45 <73508583+oka45@users.noreply.github.com>
2024-02-01 08:12:42 +0900

    Revert "管理ユーザー機能実装 (#2)" (#3)
    This reverts commit 3ad1142e4e838bc34680c65da6268fbfd62a809d.

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -22,7 +22,6 @@ class UserFactory extends Factory
     {
         return [
             'name' => fake()->name(),
-            'is_admin' => false,
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
