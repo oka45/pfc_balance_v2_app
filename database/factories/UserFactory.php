edeb89224d61ad29ae2f665efbc68e8ee16f6014
okachan45 <73508583+oka45@users.noreply.github.com>
2024-02-01 08:27:44 +0900

    Feature/管理ユーザー機能実装v2 (#4)
    * add: 管理者権限カラムの追加
    
    * add:  管理者とダミーユーザー作成
    
    * tailwindcssの導入
    
    * 管理ユーザによる一般ユーザ削除機能実装

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -22,6 +22,7 @@ class UserFactory extends Factory
     {
         return [
             'name' => fake()->name(),
+            'is_admin' => false,
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Admin\User;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -26,6 +27,10 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // ユーザー管理画面
+    Route::get('/user', User\IndexController::class)->name('admin.user.index');
+    Route::delete('/user/{id}', User\DeleteController::class)->name('admin.user.delete');
 });
 
 require __DIR__.'/auth.php';
